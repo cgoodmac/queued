490d4cc62b91b68cc38d91e69b8059f3b45fb56d
Chris Goodmacher <cgoodmac@gmail.com>
2012-12-03 14:19:30 -0500

    changed filename to que

---
+++ lib/cs/que.rb
@@ -0,0 +1,18 @@
+class Que
+
+  def initialize  
+    @data = []
+  end
+
+  def enqueue(value)
+    @data[@data.count] = value
+  end
+
+  def dequeue
+    @data = @data[0, @data.length-1] # fake pop-off, give me a new array with the last thing chopped off. That's it!
+  end
+
+  def data
+    @data
+  end
+end
