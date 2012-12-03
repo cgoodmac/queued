2c8547fe3669ddca25a0b47adde5f7f925d5b5ab
Chris Goodmacher <cgoodmac@gmail.com>
2012-12-03 14:17:10 -0500

    changed queue model

--- lib/cs/queue.rb
+++ lib/cs/queue.rb
@@ -1,14 +1,14 @@
-class Stack
+class Queue
 
   def initialize  
     @data = []
   end
 
-  def push(value)
+  def enqueue(value)
     @data[@data.count] = value
   end
 
-  def pop
+  def dequeue
     @data = @data[0, @data.length-1] # fake pop-off, give me a new array with the last thing chopped off. That's it!
   end
 
